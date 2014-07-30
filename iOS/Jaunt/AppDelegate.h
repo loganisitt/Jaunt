b526bedc06d5b240ce4c0806d17c6f6e8db806a8
Logan Isitt <laisitt@gmail.com>
2014-07-30 02:35:02 -0400

    Added .gitignore. Added iOS project.

---
+++ iOS/Jaunt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jaunt
+//
+//  Created by Logan Isitt on 7/30/14.
+//  Copyright (c) 2014 loganisitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/Jaunt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jaunt
+//
+//  Created by Logan Isitt on 7/30/14.
+//  Copyright (c) 2014 loganisitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
