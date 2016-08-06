d2a06e9a09176128c797a6d6df2a2fbeebb8ec9d
Logan Isitt <laisitt@gmail.com>
2016-08-06 18:08:14 -0400

    Speed controller recieves data from iOS app now.

--- ESC_Controller/ESC_Controller.ino
+++ ESC_Controller/ESC_Controller.ino
@@ -1,17 +1,41 @@
 #include <Servo.h>
 
+#include <SPI.h>
+#include <EEPROM.h>
+#include <boards.h>
+#include <RBL_nRF8001.h>
+#include <stdio.h>
+#include <string.h>
+
 Servo ESC;
 
 void setup() {
+
+  ble_begin();
+  ble_set_name("Jaunt");
+  
   ESC.attach(9);
   ESC.write(95);
   delay(10000);
-  for (int i = 100; i <= 180; i+= 5) {
-    ESC.write(i);
-    delay(5000);
-  }
 }
 
-void loop() {
+char speed[3];
+int count = 0;
 
+void loop() {
+  if ( ble_available() ) {
+    Serial.println("BLE AVAILABLE");
+    while ( ble_available() ) {
+        speed[count] = ble_read();
+        count++;
+    } 
+    int dec = 0, i, j, len;
+    len = strlen(speed);
+    for (i=0; i<len; i++) {
+      dec = dec * 10 + ( speed[i] - '0' );
+    }
+    ESC.write(dec); 
+    count = 0;
+  }
+  ble_do_events();
 }
