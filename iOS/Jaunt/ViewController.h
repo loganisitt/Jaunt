679a41b7512cb37da199ea92c79aca27d863729f
Logan Isitt <laisitt@gmail.com>
2016-08-07 11:55:15 -0400

    Added some third parties. Doing some clean up.

--- iOS/Jaunt/ViewController.h
+++ iOS/Jaunt/ViewController.h
@@ -1,23 +1,17 @@
 //
 //  ViewController.h
-//  Jaunt
-//
-//  Created by Logan Isitt on 7/30/14.
 //  Copyright (c) 2014 loganisitt. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "BLE.h"
 
-@interface ViewController : UIViewController <BLEDelegate>
-{
+@interface ViewController : UIViewController <BLEDelegate> {
+    
     int speedValue;
     
     BLE *bleShield;
     UIActivityIndicatorView *activityIndicator;
 }
-
-@property (weak, nonatomic) IBOutlet UISlider *speedControl;
-@property (weak, nonatomic) IBOutlet UILabel *speedLabel;
 
 @end
--- iOS/Jaunt/ViewController.m
+++ iOS/Jaunt/ViewController.m
@@ -1,23 +1,46 @@
 //
 //  ViewController.m
-//  Jaunt
-//
-//  Created by Logan Isitt on 7/30/14.
 //  Copyright (c) 2014 loganisitt. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "WMGaugeView.h"
+
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a/255.0]
+
+#define NEUTRAL_SPEED 95.0
+
+@interface ViewController () {
+    CGPoint firstPoint;
+}
+
+@property (strong, nonatomic) IBOutlet WMGaugeView *gaugeView;
+@property (strong, nonatomic) IBOutlet UILabel *gaugeLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:255.0/255.0 green:104.0/255.0 blue:97.0/255.0 alpha:255.0/255.0];
+
+    _gaugeView.style = [WMGaugeViewStyleFlatThin new];
+    _gaugeView.maxValue = 80.0;
+    _gaugeView.scaleDivisions = 8;
+    _gaugeView.scaleSubdivisions = 5;
+    _gaugeView.scaleStartAngle = 30;
+    _gaugeView.scaleEndAngle = 280;
+    _gaugeView.showScaleShadow = NO;
+    _gaugeView.scaleFont = [UIFont fontWithName:@"AvenirNext-UltraLight" size:0.065];
+    _gaugeView.scalesubdivisionsAligment = WMGaugeViewSubdivisionsAlignmentCenter;
+    _gaugeView.scaleSubdivisionsWidth = 0.002;
+    _gaugeView.scaleSubdivisionsLength = 0.04;
+    _gaugeView.scaleDivisionsWidth = 0.007;
+    _gaugeView.scaleDivisionsLength = 0.07;
+
     bleShield = [[BLE alloc] init];
     [bleShield controlSetup];
     bleShield.delegate = self;
@@ -29,6 +52,40 @@
     [self navigationItem].rightBarButtonItem = barButton;
     
     speedValue = 95;
+
+    [self.view addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragging:)]];
+}
+
+-(void)dragging:(UIPanGestureRecognizer *)gesture {
+
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        firstPoint = [gesture locationInView:gesture.view];
+    }
+
+    CGPoint newCoord = [gesture locationInView:gesture.view];
+
+    float dY = firstPoint.y - newCoord.y;
+
+    float height = gesture.view.frame.size.height;
+
+    float percentage = dY / height;
+
+    [self updateSpeed:(1 + percentage) * speedValue];
+
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        speedValue = MAX(100, MIN((1 + percentage) * speedValue, 179));
+    }
+}
+
+-(void)gaugeUpdateTimer:(NSTimer *)timer {
+
+    __weak ViewController *weakSelf = self;
+    [_gaugeView setValue:rand()%(int)_gaugeView.maxValue
+                animated:YES
+                duration:1.6
+              completion:^(BOOL finished) {
+                  weakSelf.gaugeLabel.text = [NSString stringWithFormat:@"%.0f JPS", weakSelf.gaugeView.value];
+              }];
 }
 
 -(void) connectionTimer:(NSTimer *)timer
@@ -64,16 +121,10 @@
     self.navigationItem.leftBarButtonItem.enabled = NO;
 }
 
--(void) bleDidReceiveData:(unsigned char *)data length:(int)length
-{
-    NSLog(@"%s", __PRETTY_FUNCTION__);
-
+-(void) bleDidReceiveData:(unsigned char *)data length:(int)length {
     NSData *d = [NSData dataWithBytes:data length:length];
     NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
     NSLog(@"%@", s);
-    //    NSNumber *form = [NSNumber numberWithBool:YES];
-    
-    //    NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:s, TEXT_STR, form, FORM, nil];
 }
 
 NSTimer *rssiTimer;
@@ -84,15 +135,17 @@ NSTimer *rssiTimer;
     NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
-- (void) bleDidDisconnect
-{
-    NSLog(@"bleDidDisconnect");
-    
+- (void) bleDidDisconnect {
+
+    // TODO: Add alert
+
     [self.navigationItem.leftBarButtonItem setTitle:@"Connect"];
     [activityIndicator stopAnimating];
     self.navigationItem.leftBarButtonItem.enabled = YES;
     
     [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+
+    speedValue = NEUTRAL_SPEED;
 }
 
 -(void) bleDidConnect
@@ -100,35 +153,32 @@ NSTimer *rssiTimer;
     [activityIndicator stopAnimating];
     self.navigationItem.leftBarButtonItem.enabled = YES;
     [self.navigationItem.leftBarButtonItem setTitle:@"Disconnect"];
+
+    [self updateSpeed:speedValue];
     
     NSLog(@"bleDidConnect");
-    [self.speedControl setValue:speedValue];
-    [self speedChanged:self.speedControl];
 }
 
-- (IBAction)speedChanged:(id)sender {
-    int newSpeed = [(UISlider *) sender value];
-    
-    if (newSpeed < 100 && newSpeed > 90) {
-        NSString *s = [NSString stringWithFormat:@"%i", 95];
-        NSData *d = [s dataUsingEncoding:NSUTF8StringEncoding];
-        
-        if (bleShield.activePeripheral.state == CBPeripheralStateConnected) {
-            [bleShield write:d];
-        }
-        
-        [self.speedLabel setText:s];
+- (void)updateSpeed:(int)speed {
+
+    int actualSpeed = MAX(100, MIN(speed, 179));
+
+    NSString *s = [NSString stringWithFormat:@"%i", actualSpeed];
+    NSData *d = [s dataUsingEncoding:NSUTF8StringEncoding];
+
+    if (bleShield.activePeripheral.state == CBPeripheralStateConnected) {
+        [bleShield write:d];
     }
-    else {
-        NSString *s = [NSString stringWithFormat:@"%i", newSpeed];
-        NSData *d = [s dataUsingEncoding:NSUTF8StringEncoding];
-        
-        if (bleShield.activePeripheral.state == CBPeripheralStateConnected) {
-            [bleShield write:d];
-        }
-        
-        [self.speedLabel setText:s];
-    }
+
+    __weak ViewController *weakSelf = self;
+    [_gaugeView setValue:actualSpeed % 100
+                animated:YES
+                duration:0
+              completion:^(BOOL finished) {
+                  weakSelf.gaugeLabel.text = [NSString stringWithFormat:@"%.0f JPS", weakSelf.gaugeView.value];
+              }];
+
+    NSLog(@"Speed Value: %d", actualSpeed);
 }
 
 @end
